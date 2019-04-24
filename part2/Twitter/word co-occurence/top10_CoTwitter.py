f28881768cfe07d9ddefd3d6e97c98c47598a504
Amit Banerjee <amit_b@live.com>
2019-04-24 02:00:07 -0400

    Adding project files

---
+++ part2/Twitter/word co-occurence/top10_CoTwitter.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+dictionary=defaultdict(int)
+with open('reducer_output/part-00000.txt','r') as f:
+        for line in f:
+            temp=line.split('\t')
+            dictionary[temp[0]]=int(temp[1])
+counter=0
+
+with open('top10_CoTwitter.txt','w') as f:
+    for w in sorted(dictionary, key=dictionary.get, reverse=True):
+        if counter==10:
+            break
+        temp=w+'\t'+str(dictionary[w])
+        print(temp)
+        f.write(temp+'\n')
+        counter+=1
+    f.close()
